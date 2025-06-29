978d61af5a889cd08d190e9008be6cc64296fc12
Guilherme <guilhermericci1445@gmail.com>
2025-06-28 23:50:47 -0300

    implement configManagement

---
+++ client/ConfigManager.cpp
@@ -0,0 +1,12 @@
+#include "ConfigManager.hpp"
+#include <algorithm>
+
+//initializes the file path
+ConfigManager::ConfigManager(const std::string& filepath): filePath_(filepath) {
+
+}
+
+// Utility function to trim leading/trailing whitespace from a string.
+std::string ConfigManager::trim(const std::string& str) {
+	
+}
